bddec6376ce961252762371dcc84094d29fc8d86
angele34 <90606365+angele34@users.noreply.github.com>
2025-04-06 07:21:51 +0800

    Add Files problems 4-7

--- exercises/CCPROG2/Files/Course_Notes/problems.c
+++ exercises/CCPROG2/Files/Course_Notes/problems.c
@@ -101,8 +101,115 @@ void Challenge_3(struct studentTag student) {
     printf("Input grade: ");
     scanf("%f", &student.grade);
 
-    fprintf(fp, "%d %s %s %.1f", student.ID, student.name.first, student.name.last, student.grade);
+    fprintf(fp, "%d %s %s %.1f\n", student.ID, student.name.first, student.name.last, student.grade);
     fclose(fp);
+}
+
+/* CHALLENGE #4: 
+Read all the contents of "STUDENTS.TXT" and produce as a result a new binary file named
+“STUDENTS.BIN” which contains the same student data from the input text file. 
+You should fscanf() the student data into a student structure variable, 
+and fwrite() it to the binary file.
+*/
+
+void Challenge_4(struct studentTag student) {
+    FILE *fp = fopen("STUDENTS.TXT", "r");
+    if (fp == NULL) {
+        printf("ERROR: File STUDENTS.TXT not found.\n");
+        exit(1);
+    }
+    FILE *output = fopen("STUDENTS.BIN", "wb");
+    if (output == NULL) {
+        printf("ERROR: Could not open STUDENTS.BIN for writing.\n");
+        fclose(fp);
+        exit(1);
+    }
+    while(fscanf(fp, "%d %s %s %f", &student.ID, student.name.first, student.name.last, &student.grade) == 4) {
+        // write student data to bin file, file pointer is output
+        fwrite(&student, sizeof(struct studentTag), 1, output);
+    }
+    fclose(fp);
+    fclose(output);
+}
+
+/* CHALLENGE #5:
+Read all the contents of the binary file “ STUDENTS.BIN” and printf() the student information. Please
+take note that you do not know in advance how many student data values are stored in the binary file.
+*/
+
+void Challenge_5(struct studentTag student) {
+    FILE *fp_bin = fopen("STUDENTS.BIN", "rb");
+    if (fp_bin == NULL) {
+        printf("ERROR: File STUDENTS.BIN not found\n");
+        exit(1);
+    }
+    printf("\n\nBinary file output:\n");
+    while (fread(&student, sizeof(struct studentTag), 1, fp_bin) == 1) {
+        printf("%d %s %s %.1f\n", student.ID, student.name.first, student.name.last, student.grade);
+    }
+    fclose(fp_bin);
+}
+
+/* CHALLENGE #6:
+Read contents of the binary file “ STUDENTS.BIN” in **reversed order** and copy them onto a new
+binary file named “STUDENTS-REV.BIN”.
+*/
+
+void Challenge_6(struct studentTag student) {
+    FILE *fp_bin = fopen("STUDENTS.BIN", "rb");
+    if (fp_bin == NULL) {
+        printf("ERROR: File STUDENTS.BIN not found\n");
+        exit(1);
+    }
+    FILE *cpy_bin = fopen("STUDENTS-REV.BIN", "wb");
+
+    fseek(fp_bin, 0, SEEK_END);
+    long file_size = ftell(fp_bin);
+    int n = file_size / sizeof(struct studentTag);
+
+    // Copy the contents of STUDENTS.BIN in reverse
+    for (int i = n-1; i >= 0; i--) {
+        fseek(fp_bin, i*sizeof(struct studentTag), SEEK_SET);
+        fread(&student, sizeof(struct studentTag), 1, fp_bin);
+        fwrite(&student, sizeof(struct studentTag), 1, cpy_bin);
+    }
+    fclose(fp_bin);
+    fclose(cpy_bin);
+}
+
+/* CHALLENGE 7:
+Perform linear search on the “STUDENTS.BIN” binary file. The user will input the ID number. 
+If the ID number is found, the program should print the information about the student.
+Otherwise, it should output “NOT FOUND!” 
+The search will be repeated until the user inputs a sentinel value of -999, in which case the program outputs
+“END SEARCH”.
+*/
+
+void Challenge_7(struct studentTag student) {
+    FILE *fp_bin = fopen("STUDENTS.BIN", "rb");
+    if (fp_bin == NULL) {
+        printf("ERROR: File STUDENTS.BIN not found\n");
+        exit(1);
+    }
+    int ID;
+    do {
+        printf("\nInput ID for search: ");
+        scanf("%d", &ID);
+        fseek(fp_bin, 0, SEEK_SET); // reset file pointer
+        int found = 0;
+        while (fread(&student, sizeof(struct studentTag), 1, fp_bin) == 1) {
+            if (ID == student.ID) {
+                printf("%d %s %s %.1f\n", student.ID, student.name.first, student.name.last, student.grade);
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            printf("NOT FOUND!\n");
+    } while (ID != -999);
+
+    printf("END SEARCH\n");
+    fclose(fp_bin);
 }
 
 int main() {
@@ -110,5 +217,9 @@ int main() {
     Challenge_1(student);
     Challenge_2(student);
     Challenge_3(student);
+    Challenge_4(student);
+    Challenge_5(student);
+    Challenge_6(student);
+    Challenge_7(student);
     return 0;
 }
