b986e2339789287436819766af7d9fbb1b54d31e
angele34 <90606365+angele34@users.noreply.github.com>
2025-09-15 20:39:20 +0800

    Add Stratascratch - Salaries Difference

---
+++ unclassified/SQL Practice/Stratascratch/SalariesDifference.sql
@@ -0,0 +1,13 @@
+-- https://platform.stratascratch.com/coding/10308-salaries-differences
+
+SELECT ABS(
+    (SELECT MAX(salary) 
+    FROM db_employee 
+    WHERE department_id = 
+    (SELECT id FROM db_dept WHERE department = 'marketing'))
+    -
+    (SELECT MAX(salary) 
+    FROM db_employee 
+    WHERE department_id = 
+    (SELECT id FROM db_dept WHERE department = 'engineering'))
+) AS salary_difference;
